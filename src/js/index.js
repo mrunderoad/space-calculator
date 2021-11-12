898584b6f632886c4ddeef81d7d75f5708f1751b
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-12 15:24:38 -0800

    Added alerts to test calculations

--- src/js/index.js
+++ src/js/index.js
@@ -24,24 +24,29 @@ function earthToJupiter(years) {
   }
 }
 
+function lifeLeftEarth() {
+  return lifeExpectancy - years;
+}
+
+
 function lifeLeftMercury() {
-  return lifeExpectancy - earthToMercury(age);
+  return earthToMercury(lifeExpectancy) - earthToMercury(years);
+}
+
+function lifeLeftVenus() {
+  return earthToVenus(lifeExpectancy) - earthToVenus(years);
+}
+
+function lifeLeftMars() {
+  return earthToMars(lifeExpectancy) - earthToMars(years);
+}
+
+function lifeLeftJupiter() {
+  return earthToJupiter(lifeExpectancy) - earthToJupiter(years);
 }
 
 const lifeExpectancy = parseInt(prompt("How many Earth years do you want to live?"));
-const age = parseInt(prompt("Enter your age on Earth:"));
- alert("Your age on Mercury is " + earthToMercury(years) + "! And your life expectancy on Mercury is " + earthToMercury(lifeExpectancy) + " years. "); //+ "Your age on Venus is " + earthToVenus(years) + "! And your life expectancy on Venus is " + earthToVenus(lifeExpectancy) + " years. " + "Your age on Mars is: " + earthToMars(years) + "! And your life expectancy on Mars is " + earthToMars(lifeExpectancy) + " years. " + "Your age on Jupiter is: " + earthToJupiter(years) + "! And your life expectancy on Jupiter is " + earthToJupiter(lifeExpectancy) + " years.");
+const years = parseInt(prompt("Enter your age on Earth:"));
+ alert("Your age on Mercury is " + earthToMercury(years) + " with a life expectancy of " + earthToMercury(lifeExpectancy) + " years. " + "Your age on Venus is " + earthToVenus(years) + " with a life expectancy of " + earthToVenus(lifeExpectancy) + " years. " + "Your age on Mars is: " + earthToMars(years) + " with a life expectancy of " + earthToMars(lifeExpectancy) + " years. " + "Your age on Jupiter is: " + earthToJupiter(years) + " with a life expectancy of " + earthToJupiter(lifeExpectancy) + " years.");
 
-alert("You have " + lifeLeftMercury(years) + " years left to live on this planet");
-
-
-  
-
-
-
-
-
-// one year on mercury is .24 earth years --- so one year on earth is 4.16 mercury years
-// one year on venus is .62 earth years --- so one year on earth is 1.61 venus years
-// one year on mars is 1.88 earth years --- so one year on earth is .53 mars years
-// one year on jupiter is 11.86 earth years --- so one year on earth is .0084 jupiter years
+alert("You have " + lifeLeftMercury(years) + " years left to live on Mercury in Mercury years. You have " + lifeLeftVenus(years) + " years left on Venus in Venus years. You have " + lifeLeftMars(years) + " years left on Mars in Mars years. You have " + lifeLeftJupiter(years) + " years left on Jupiter. With a total of " + lifeLeftEarth(years) + " on Earth!" );
--- src/js/space.js
+++ src/js/space.js
@@ -1,5 +1,19 @@
-export default function earthToMercury(oneEarthYear) {
-  if (oneEarthYear === 1) {
-  return "4.16";
-  }
-  }
+export function earthToMercury(years) {
+  let mercuryAge = age / .24;
+}
+
+export function earthToVenus(years) {
+  let venusAge = age / .62;
+}
+
+export function earthToMars(years) {
+  let marsAge = age / 1.88;
+}
+
+export function earthToJupiter(years) {
+  let jupiterAge = age / .0084;
+}
+
+export function lifeLeftMercury(years) {
+  return earthToMercury(lifeExpectancy) - earthToMercury(years); 
+}

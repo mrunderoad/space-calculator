eae2d99bb6c467932a4846ae7656f2f69c797bb0
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-12 15:36:53 -0800

    Fixed placement of functions from index.js to space.js

--- src/js/space.js
+++ src/js/space.js
@@ -1,19 +1,45 @@
 export function earthToMercury(years) {
-  let mercuryAge = age / .24;
+  return years / .24; {
+  }
 }
 
 export function earthToVenus(years) {
-  let venusAge = age / .62;
+  return years / .62; { 
+  }
 }
 
 export function earthToMars(years) {
-  let marsAge = age / 1.88;
+  return years / 1.88; {
+  }  
 }
 
 export function earthToJupiter(years) {
-  let jupiterAge = age / .0084;
+  return years / .0084; {    
+  }
 }
 
-export function lifeLeftMercury(years) {
-  return earthToMercury(lifeExpectancy) - earthToMercury(years); 
+function lifeLeftEarth() {
+  return lifeExpectancy - years;
 }
+
+export function lifeLeftMercury() {
+  return earthToMercury(lifeExpectancy) - earthToMercury(years);
+}
+
+function lifeLeftVenus() {
+  return earthToVenus(lifeExpectancy) - earthToVenus(years);
+}
+
+function lifeLeftMars() {
+  return earthToMars(lifeExpectancy) - earthToMars(years);
+}
+
+function lifeLeftJupiter() {
+  return earthToJupiter(lifeExpectancy) - earthToJupiter(years);
+}
+
+const lifeExpectancy = parseInt(prompt("How many Earth years do you want to live?"));
+const years = parseInt(prompt("Enter your age on Earth:"));
+ alert("Your age on Mercury is " + earthToMercury(years) + " with a life expectancy of " + earthToMercury(lifeExpectancy) + " years. " + "Your age on Venus is " + earthToVenus(years) + " with a life expectancy of " + earthToVenus(lifeExpectancy) + " years. " + "Your age on Mars is: " + earthToMars(years) + " with a life expectancy of " + earthToMars(lifeExpectancy) + " years. " + "Your age on Jupiter is: " + earthToJupiter(years) + " with a life expectancy of " + earthToJupiter(lifeExpectancy) + " years.");
+
+alert("You have " + lifeLeftMercury(years) + " years left to live on Mercury in Mercury years. You have " + lifeLeftVenus(years) + " years left on Venus in Venus years. You have " + lifeLeftMars(years) + " years left on Mars in Mars years. You have " + lifeLeftJupiter(years) + " years left on Jupiter. With a total of " + lifeLeftEarth(years) + " on Earth!" );

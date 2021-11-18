75aeeb9f430479e9a29c3a6fd82f724ad03341a7
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-18 15:22:52 -0800

    Rewriting tests, first and second passing

--- __tests__/space.tests.js
+++ __tests__/space.tests.js
@@ -1,54 +1,67 @@
 
-import { EarthToMercury, EarthToVenus, EarthToMars, EarthToJupiter, LifeLeftMercury, YearsOverExpected  } from './../src/js/space.js';
+import { SpaceCalculator } from './../src/js/space.js';
 
-test('Should correctly determine the length of one mercury year', () => {
-  let oneMercuryYear = 4.15;
-});
+describe('SpaceCalculator', () => {
+  let spaceCalculator;
+  beforeEach(()=> {
+  spaceCalculator = new SpaceCalculator(30,80)
+  })
 
-test('Should correctly determine the length of one venus year', () => {
-  let oneVenusYear = 1.61;
-});
+  test("should create a new instance of spaceCalculator", () => {
+    expect(spaceCalculator.years).toEqual(30);
+    expect(spaceCalculator.lifeExpectancy).toEqual(80);
+  });
 
-test('Should correctly determine the length of one Mars year', () => {
-  let oneMarsYear = 1.88;
-});
+   test('Should correctly convert earth years into mercury years', () => {
+     expect(spaceCalculator.earthToMercury(1)).toEqual(4.166666666666667)
+   });
 
-test('Should correctly determine the length of one Jupiter year', () => {
-  let oneJupiterYear = .0084;
-});
+  // test('Should correctly determine the length of one venus year', () => {
+  //   let oneVenusYear = 1.61;
+  // });
 
-test('Should convert earth age to mercury age', () => {
-  let mercuryAge = 4.16;
-  let earthAge = {};
-  earthAge / mercuryAge;
-});
+  // test('Should correctly determine the length of one Mars year', () => {
+  //   let oneMarsYear = 1.88;
+  // });
 
-test('Should convert earth age to Venus age', () => {
-  let venusAge = 1.61;
-  let earthAge = {};
-  earthAge / venusAge;
-});
+  // test('Should correctly determine the length of one Jupiter year', () => {
+  //   let oneJupiterYear = .0084;
+  // });
 
-test('Should convert earth age to Mars age', () => {
-  let marsAge = 1.88;
-  let earthAge = {};
-  earthAge / marsAge;
-});
+  // test('Should convert earth age to mercury age', () => {
+  //   let mercuryAge = 4.16;
+  //   let earthAge = {};
+  //   earthAge / mercuryAge;
+  // });
 
-test('Should convert earth age to Jupiter age', () => {
-  let jupiterAge = .0084;
-  let earthAge = {};
-  earthAge / jupiterAge;
-});
+  // test('Should convert earth age to Venus age', () => {
+  //   let venusAge = 1.61;
+  //   let earthAge = {};
+  //   earthAge / venusAge;
+  // });
 
-test('Should tell user their life left on planet and on earth', () => {
-  let year = {};
-  let lifeExpectancy = {};
-  year - lifeExpectancy;
-});
+  // test('Should convert earth age to Mars age', () => {
+  //   let marsAge = 1.88;
+  //   let earthAge = {};
+  //   earthAge / marsAge;
+  // });
 
-test('Should tell user how may years over their expected life they are', () => {
-  let year = {};
-  let lifeExpectancy = {};
-  lifeExpectancy - year;
+  // test('Should convert earth age to Jupiter age', () => {
+  //   let jupiterAge = .0084;
+  //   let earthAge = {};
+  //   earthAge / jupiterAge;
+  // });
+
+  // test('Should tell user their life left on planet and on earth', () => {
+  //   let year = {};
+  //   let lifeExpectancy = {};
+  //   year - lifeExpectancy;
+  // });
+
+  // test('Should tell user how may years over their expected life they are', () => {
+  //   let year = {};
+  //   let lifeExpectancy = {};
+  //   lifeExpectancy - year;
+  // });
+
 });
--- src/js/space.js
+++ src/js/space.js
@@ -1,59 +1,56 @@
-export function earthToMercury(years) {
-  return years / .24; {
+export class SpaceCalculator {
+  constructor(years, lifeExpectancy){
+    this.years = years;
+    this.lifeExpectancy = lifeExpectancy;
   }
-}
 
-export function earthToVenus(years) {
-  return years / .62; { 
+  earthToMercury(years) {
+    return years / .24; 
   }
-}
-
-export function earthToMars(years) {
-  return years / 1.88; {
-  }  
-}
-
-export function earthToJupiter(years) {
-  return years / .0084; {    
+  
+  earthToVenus(years) {
+    return years / .62;  
   }
-}
-
-function lifeLeftEarth() {
-  return lifeExpectancy - years; {
+  
+  earthToMars(years) {
+    return years / 1.88;   
   }
-}
-
-export function lifeLeftMercury() {
-  return earthToMercury(lifeExpectancy) - earthToMercury(years); {
+  
+  earthToJupiter(years) {
+    return years / .0084; 
   }
-}
-
-function lifeLeftVenus() {
-  return earthToVenus(lifeExpectancy) - earthToVenus(years); {
+  
+  lifeLeftEarth() {
+    return this.lifeExpectancy - this.years; 
   }
-}
-
-function lifeLeftMars() {
-  return earthToMars(lifeExpectancy) - earthToMars(years); {
+  
+  lifeLeftMercury() {
+    return earthToMercury(this.lifeExpectancy) - earthToMercury(this.years); 
   }
-}
-
-function lifeLeftJupiter() {
-  return earthToJupiter(lifeExpectancy) - earthToJupiter(years); {
+  
+  lifeLeftVenus() {
+    return earthToVenus(this.lifeExpectancy) - earthToVenus(this.years); 
   }
-}
-
-export function yearsOverExpected() {
-  return years - lifeExpectancy; {
+  
+  lifeLeftMars() {
+    return earthToMars(this.lifeExpectancy) - earthToMars(this.years); 
   }
-}
+  
+  lifeLeftJupiter() {
+    return earthToJupiter(this.lifeExpectancy) - earthToJupiter(this.years); 
+  }
+  
+  yearsOverExpected() {
+    return this.years - this.lifeExpectancy; 
+  }
+} 
 
-const lifeExpectancy = parseInt(prompt("How many Earth years do you want to live?"));
-const years = parseInt(prompt("Enter your age on Earth:"));
- if (years > lifeExpectancy) { 
-   alert("C'mon now, thats unrealistic, you must be a zombie! You've been dead for " + yearsOverExpected(years) + " years!")
- } else {
- alert("Your age on Mercury is " + earthToMercury(years) + " with a life expectancy of " + earthToMercury(lifeExpectancy) + " years. " + "Your age on Venus is " + earthToVenus(years) + " with a life expectancy of " + earthToVenus(lifeExpectancy) + " years. " + "Your age on Mars is: " + earthToMars(years) + " with a life expectancy of " + earthToMars(lifeExpectancy) + " years. " + "Your age on Jupiter is: " + earthToJupiter(years) + " with a life expectancy of " + earthToJupiter(lifeExpectancy) + " years.");
+// const lifeExpectancy = parseInt(prompt("How many Earth years do you want to live?"));
+// const years = parseInt(prompt("Enter your age on Earth:"));
+//  if (years > lifeExpectancy) { 
+//    alert("C'mon now, thats unrealistic, you must be a zombie! You've been dead for " + yearsOverExpected(years) + " years!")
+//  } else {
+//  alert("Your age on Mercury is " + earthToMercury(years) + " with a life expectancy of " + earthToMercury(lifeExpectancy) + " years. " + "Your age on Venus is " + earthToVenus(years) + " with a life expectancy of " + earthToVenus(lifeExpectancy) + " years. " + "Your age on Mars is: " + earthToMars(years) + " with a life expectancy of " + earthToMars(lifeExpectancy) + " years. " + "Your age on Jupiter is: " + earthToJupiter(years) + " with a life expectancy of " + earthToJupiter(lifeExpectancy) + " years.");
 
-alert("You have " + lifeLeftMercury(years) + " years left to live on Mercury in Mercury years. You have " + lifeLeftVenus(years) + " years left on Venus in Venus years. You have " + lifeLeftMars(years) + " years left on Mars in Mars years. You have " + lifeLeftJupiter(years) + " years left on Jupiter. With a total of " + lifeLeftEarth(years) + " left on Earth!" );
- };
+// alert("You have " + lifeLeftMercury(years) + " years left to live on Mercury in Mercury years. You have " + lifeLeftVenus(years) + " years left on Venus in Venus years. You have " + lifeLeftMars(years) + " years left on Mars in Mars years. You have " + lifeLeftJupiter(years) + " years left on Jupiter. With a total of " + lifeLeftEarth(years) + " left on Earth!" );
+//  };
